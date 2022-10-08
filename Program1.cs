1304cc56de462c29f4d57efafde40f01bb189f36
unknown <ys25q>
2022-10-08 12:54:42 +0300

    Cohngkjh

---
+++ Program1.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+void PrintNumber() {
+    float nmax;
+    Console.WriteLine("1 number");
+    float a = float.Parse(Console.ReadLine());
+    Console.WriteLine("2 number");
+    float b = float.Parse(Console.ReadLine());
+    { if (a > b) ;
+        nmax = a;
+        if (a < b) ;
+        nmax = b; }
+    Console.WriteLine(nmax);
+}
+
+PrintNumber();
